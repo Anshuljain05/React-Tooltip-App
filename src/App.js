3c6e44099f22ce37ea5c1cb2410b7c8dc327865e
Anshuljain05 <anshuljain05@outlook.com>
2022-09-28 14:41:37 +0530

    React Tooltip App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+import Tooltip from "./components/tooltip";
+import TooltipSelector from "./components/selector/ttselector";
+
+const TooltipTarget = styled.span`
+color: #fff;
+`;
 
 function App() {
+  const [position, setPosition] = useState("left");
+
+  const handleChange = (value) => {
+    setPosition(value);
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TooltipSelector onChange={handleChange} />
+      <Tooltip text="I am a tooltip" position={position} background="00adb5">
+        <TooltipTarget>Hover over me!</TooltipTarget>
+      </Tooltip>
+      {/* <Tooltip text="I am a tooltip" position="top" background="222831">
+        <TooltipTarget>Top</TooltipTarget>
+        </Tooltip>
+        <Tooltip text="I am a tooltip" position="bottom" background="393e46">
+        <TooltipTarget>Bottom</TooltipTarget>
+        </Tooltip>
+        <Tooltip text="I am a tooltip" position="right" background="eeeeee">
+        <TooltipTarget>Right</TooltipTarget>
+      </Tooltip> */}
+      {/* <p>
+        This is a{" "}
+        <Tooltip text="I am within paragraph" position="top" styleMe={false}>
+        <TooltipTarget>tooltip</TooltipTarget>
+        </Tooltip>{" "}
+        within a paragraph.
+      </p> */}
     </div>
   );
 }
